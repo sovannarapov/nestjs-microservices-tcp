fc84f132ba0c75942ec3e31a9d567bb6cd6f7de8
sovannarapov <sovannarapov9@gmail.com>
2024-09-13 09:33:44 +0700

    refactor: update app module

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,12 +1,48 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { DataSource } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import database from './config/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Makes the configuration module globally available
-      envFilePath: '.env', // Optionally specify a path to the .env file
+      isGlobal: true,
+      envFilePath: ['.env'],
+      load: [database],
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 400,
+      },
+    ]),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService) => configService.get('database'),
+      dataSourceFactory: async (options) => {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+
+        const dataSource = new DataSource(options);
+
+        try {
+          if (!dataSource.isInitialized) {
+            await dataSource.initialize();
+          }
+
+          return addTransactionalDataSource(dataSource);
+        } catch {
+          /* empty */
+        }
+
+        return dataSource;
+      },
     }),
     UserModule,
   ],
