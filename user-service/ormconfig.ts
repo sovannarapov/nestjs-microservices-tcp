b6a4012a6bcb0b1e9229b88f83c9108f32b77f12
sovannarapov <sovannarapov9@gmail.com>
2024-09-13 11:11:46 +0700

    fix: fix port possibly undefined

--- user-service/ormconfig.ts
+++ user-service/ormconfig.ts
@@ -1,7 +1,9 @@
+import { toInteger } from 'lodash';
+
 module.exports = {
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
+  port: toInteger(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE || '',
